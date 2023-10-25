d4e48adf55eaceed90a168f2c4d17b6677c0d800
Andrezimdopvp <102673694+Andrezimdopvp@users.noreply.github.com>
2023-10-25 19:12:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function volta(){
+    window.location="index.html";
+}
+function doenca(){
+    window.location="doencas.html";
+}
+function composicao(){
+    window.location="vacina.html";
+}
+function requerido(){
+    window.location="requerido.html";
+}
